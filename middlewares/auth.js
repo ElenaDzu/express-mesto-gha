3d5785dc5fc2e7fe0311758d8256356b1ad95d40
ElenaDzu <turin78952@mail.ru>
2022-09-12 14:31:50 +0300

    fixing autotests

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,33 @@
 const jwt = require('jsonwebtoken');
+
+const handleAuthError = (res) => {
+  res
+    .status(401)
+    .send({ message: 'Необходима авторизация' });
+};
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return handleAuthError(res);
+  }
+
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+
+  try {
+    payload = jwt.verify(token, 'super-strong-secret');
+  } catch (err) {
+    return handleAuthError(res);
+  }
+
+  req.user = payload; // записываем пейлоуд в объект запроса
+
+  next(); // пропускаем запрос дальше
+};
+
+/*
 const Unauthorized401 = require('../Errors/Unauthorized 401');
 
 module.exports = (req, res, next) => {
@@ -21,3 +50,4 @@ module.exports = (req, res, next) => {
 
   next();
 };
+*/
