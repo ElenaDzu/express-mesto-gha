560daf1defffcf4d8b69eef1eab1d157f49b1b2c
ElenaDzu <turin78952@mail.ru>
2022-09-12 14:27:56 +0300

    fixing autotests

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new Unauthorized401();
+    throw new Unauthorized401(res);
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -14,7 +14,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    throw new Unauthorized401();
+    throw new Unauthorized401(res);
   }
 
   req.user = payload;
