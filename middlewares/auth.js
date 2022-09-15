080b3193b119527aeaa78ba7678e3c63a0adf7ba
ElenaDzu <turin78952@mail.ru>
2022-09-15 15:25:14 +0300

    fixing errors

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,14 +5,14 @@ const Unauthorized401 = require('../Errors/Unauthorized 401');
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    if (!req.cookie('token')) { throw new Unauthorized401('Неверный логин, пароль, токен'); }
+    if (!req.cookie.token) { throw new Unauthorized401('Неверный логин, пароль, токен'); }
   }
   let token;
 
   if (authorization) {
     token = authorization.replace('Bearer ', '');
   } else {
-    token = req.cookie;
+    token = req.cookie.token;
   }
 
   let payload;
