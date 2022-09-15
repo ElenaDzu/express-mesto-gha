0ffccffcbead5808443900bbef04744f0bf199c0
ElenaDzu <turin78952@mail.ru>
2022-09-15 14:48:30 +0300

    fixing errors

--- app.js
+++ app.js
@@ -28,10 +28,6 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().regex(
-      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/,
-    ),
   }),
 }), createUser);
 
