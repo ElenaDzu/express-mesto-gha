9835d3ffd831c40cce281a1f2e4a7c99e0849b5f
ElenaDzu <turin78952@mail.ru>
2022-09-15 18:42:31 +0300

    auth

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const { errors, celebrate, Joi } = require('celebrate');
+const router = require('express').Routrer();
 const routes = require('./routes/users');
 const NotFound404 = require('./Errors/NotFound404');
 const { login, createUser } = require('./controllers/users');
@@ -18,14 +19,14 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
-app.post('/signin', auth, celebrate({
+app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
 }), login);
 
-app.post('/signup', auth, celebrate({
+app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     email: Joi.string().required().email(),
@@ -36,7 +37,7 @@ app.post('/signup', auth, celebrate({
     ),
   }),
 }), createUser);
-
+router.use(auth);
 app.use('/users', routes);
 
 app.use('/cards', require('./routes/cards'));
