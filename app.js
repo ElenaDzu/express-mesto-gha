b0d558773d81f4014c0b2c52b1717ef42aa96a67
ElenaDzu <turin78952@mail.ru>
2022-09-15 23:18:31 +0300

    routes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
@@ -9,6 +10,8 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(routes);
 mongoose.connect('mongodb://localhost:27017/mestodb', {
